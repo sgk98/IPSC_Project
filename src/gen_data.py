7e9205652714f48df37d8f35d4acbc5afe8a1457
sgk98 <shyamgopal@protonmail.com>
2020-03-20 08:20:29 +0530

    Distributed SGD(Buggy) added

--- src/gen_data.py
+++ src/gen_data.py
@@ -1,10 +1,23 @@
 import numpy as np
 from sklearn.datasets import make_classification
+import random
 
-samples=make_classification(n_samples=20000,n_features=500,n_informative=400)
-
-
+num_samples=20000
+num_features=500
+samples=make_classification(n_samples=num_samples,n_features=num_samples,n_informative=400)
 X=samples[0]
 Y=samples[1]
-np.savetxt('train.txt',X)
-np.savetxt('labels.txt',Y,fmt="%d")
+
+#Randomly shuffling samples for distributed SGD
+random_order=[i for i in range(20000)]
+random.shuffle(random_order)
+finalX=[]
+finalY=[]
+for i in random_order:
+	finalX.append(X[i])
+	finalY.append(Y[i])
+
+
+
+np.savetxt('train.txt',finalX)
+np.savetxt('labels.txt',finalY,fmt="%d")
