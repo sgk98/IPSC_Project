e6a8c5b51cba5d546e5a638244e260d4cf9d95ff
iiit <shyamgopal@protonmail.com>
2020-03-20 11:02:49 +0530

    Revert to old generator script

--- src/gen_data.py
+++ src/gen_data.py
@@ -9,15 +9,17 @@ X=samples[0]
 Y=samples[1]
 
 #Randomly shuffling samples for distributed SGD
+'''
 random_order=[i for i in range(20000)]
 random.shuffle(random_order)
 finalX=[]
 finalY=[]
 for i in random_order:
+	print(i)
 	finalX.append(X[i])
 	finalY.append(Y[i])
 
+'''
 
-
-np.savetxt('train.txt',finalX)
-np.savetxt('labels.txt',finalY,fmt="%d")
+np.savetxt('train.txt',X)
+np.savetxt('labels.txt',Y,fmt="%d")
